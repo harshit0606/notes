98f2226d7ec0cd1ed27d464916f628da36cfdf3b
Harshit Pathak <harshitasr001@gmail.com>
2022-09-05 15:23:56 +0530

    all done

--- src/components/notes/notes.js
+++ src/components/notes/notes.js
@@ -1,10 +1,13 @@
-import React, { useEffect, useState , useMemo } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 
 import Pagination from "../pagination/Pagination";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { Modal } from "react-bootstrap";
 import { BsPin } from "react-icons/bs";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import SingleNote from "./singleNote";
 import "./notes.css";
 
@@ -13,30 +16,47 @@ function Notes() {
   const [title, setTitle] = useState("");
   const [tagline, setTagline] = useState("");
   const [text, setText] = useState("");
-  const [update,setUpdate]=useState(false);
-  const [modalShow,setModalShow]=useState(false);
+  const [update, setUpdate] = useState(false);
+  const [modalShow, setModalShow] = useState(false);
   const [ispinned, setisPinned] = useState(false);
-  const [loading,setLoading]=useState(true);
-  let view=useSelector((state)=>state.notes);
-let theme=useSelector((state)=>state.theme);
-let PageSize = 6;
-const [currentPage, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  let view = useSelector((state) => state.notes);
+  let theme = useSelector((state) => state.theme);
+  let PageSize = 6;
+  const [currentPage, setCurrentPage] = useState(1);
 
-const currentTableData = useMemo(() => {
-  const firstPageIndex = (currentPage - 1) * PageSize;
-  const lastPageIndex = firstPageIndex + PageSize;
-  return notes.slice(firstPageIndex, lastPageIndex);
-}, [currentPage,loading]);
+  const notify = () => toast.error("Cannot add an empty note!");
 
-function handleModalClose() {
-  console.log("closing");
-  setModalShow(false);
-}
+  const notify1 = () => toast.error("Cannot leave a note empty!");
 
-function handleOpen() {
-  setModalShow(true);
-}
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return notes.sort((a,b) => new Date(b.createdAt) - new Date(a.createdAt))
+    .sort((a,b) => {
+      return (b.isPinned - a.isPinned)
+    })
+    .slice(firstPageIndex, lastPageIndex)
+  }, [currentPage, loading, notes]);
 
+  function handleModalClose() {
+    console.log("closing");
+    setModalShow(false);
+  }
+
+  function handleOpen() {
+    setModalShow(true);
+  }
+
+  function handleDelete(noteId) {
+    axios.delete(`http://localhost:5000/api/deletenote/${noteId}`).then(() => {
+      console.log("Note deleted");
+      let b = notes.findIndex((a) => a._id === noteId);
+      var dummynotes = [...notes];
+      dummynotes.splice(b, 1);
+      setNotes(dummynotes);
+    });
+  }
 
   useEffect(() => {
     setNotes([]);
@@ -49,7 +69,6 @@ function handleOpen() {
       .catch((err) => {
         console.log(err);
       });
-    
 
     let inputs = document.querySelector(".add_inputs");
     let input1 = document.querySelector(".add_input");
@@ -69,18 +88,18 @@ function handleOpen() {
   function handleClose() {
     let inputs = document.querySelector(".add_inputs");
     let input1 = document.querySelector(".add_input");
-    if(!theme){
-      input1=document.querySelector(".add_input_dark");
+    if (!theme) {
+      input1 = document.querySelector(".add_input_dark");
     }
-    let pin = document.querySelector(".pin_icon");
-    if (inputs && pin && input1) {
+ 
+    if (inputs  && input1) {
       input1.placeholder = "Take a note...";
       inputs.classList.remove("visible");
-      pin.classList.remove("visible");
+    
     }
     setTagline("");
-        setText("");
-        setTitle("");
+    setText("");
+    setTitle("");
   }
   function handleChange(e) {
     e.preventDefault();
@@ -95,10 +114,25 @@ function handleOpen() {
       setText(value);
     }
   }
-  function handlePin() {}
-
-  function handleAdd() {
+  function handlePin(note) {
     axios
+      .patch(`http://localhost:5000/api/editnote/${note._id}`, {
+        isPinned: note.isPinned ? false : true,
+      })
+      .then(() => {
+        let b = notes.findIndex((a) => a._id === note._id);
+        var dummynotes = [...notes];
+        dummynotes[b].isPinned = note.isPinned ? false : true;
+        setNotes(dummynotes);
+      });
+  }
+  function handleAdd() {
+
+if(title==="" && tagline==="" && text===""){
+  notify();
+}
+else{
+  axios
       .post("http://localhost:5000/api/addnote", {
         Title: title,
         Tagline: tagline,
@@ -112,14 +146,17 @@ function handleOpen() {
         setTitle("");
         handleClose();
       });
+}
+  
   }
 
   return (
-    <div className={theme?"allNotes":"allNotes_dark"}>
+    <div className={theme ? "allNotes" : "allNotes_dark"}>
+    <ToastContainer />
       <div className="add_note">
-        <BsPin onClick={handlePin} className="pin_icon" />
+       
         <input
-          className={theme?"add_input":'add_input_dark'}
+          className={theme ? "add_input" : "add_input_dark"}
           type="text"
           name="title"
           onChange={(e) => {
@@ -159,19 +196,32 @@ function handleOpen() {
           </div>
         </div>
       </div>
-      
-      <div className={view?"all_notes":"all_notes full"}>
+
+      <div className={view ? "all_notes" : "all_notes full"}>
         {currentTableData.map((note) => (
-          <SingleNote update={update} setUpdate={setUpdate} openModal={handleOpen} close={handleModalClose} note={note} />
+          <SingleNote
+            pinNote={handlePin}
+            deletee={handleDelete}
+            update={update}
+            setUpdate={setUpdate}
+            openModal={handleOpen}
+            close={handleModalClose}
+            notes={notes}
+            notify1={notify1}
+            setNotes={setNotes}
+            note={note}
+          />
         ))}
       </div>
-      <Pagination
-        className="pagination-bar"
-        currentPage={currentPage}
-        totalCount={notes.length}
-        pageSize={PageSize}
-        onPageChange={page => setCurrentPage(page)}
-      />
+      <div style={{ marginLeft: "auto", marginRight: "auto" }}>
+        <Pagination
+          className="pagination-bar"
+          currentPage={currentPage}
+          totalCount={notes.length}
+          pageSize={PageSize}
+          onPageChange={(page) => setCurrentPage(page)}
+        />
+      </div>
     </div>
   );
 }
--- src/components/notes/singleNote.js
+++ src/components/notes/singleNote.js
@@ -6,46 +6,39 @@ import { useSelector } from "react-redux";
 import { IoTrashOutline } from "react-icons/io5";
 import { Modal } from "react-bootstrap";
 import { FaCloudShowersHeavy } from "react-icons/fa";
- 
-  
-   
-  
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function SingleNote({ note ,openModal, close,update,setUpdate}) {
+function SingleNote({
+  note,
+  pinNote,
+  deletee,
+  notes,
+  setNotes,
+  notify1
+}) {
   const [title, setTitle] = useState("");
   const [tagline, setTagline] = useState("");
   const [text, setText] = useState("");
   const [ispinned, setisPinned] = useState(false);
-  const [modalShow,setModalShow]=useState(false);
+  const [modalShow, setModalShow] = useState(false);
   let view = useSelector((state) => state.notes);
   let theme = useSelector((state) => state.theme);
 
-  function handleDelete() {
-    axios
-      .delete(`http://localhost:5000/api/deletenote/${note._id}`)
-      .then(() => {
-        console.log("Note deleted");
-        if(update==true){
-          setUpdate(false);
-        }else{
-          setUpdate(true);
-        }
-      });
-  }
 
- 
+  
+
   function handleModalClose() {
     console.log("closing");
     setModalShow(false);
   }
-  
+
   function handleOpen(note) {
     setModalShow(true);
     setTitle(note.Title);
     setTagline(note.Tagline);
     setText(note.Text);
   }
-  
 
   function handleChange(e) {
     e.preventDefault();
@@ -60,119 +53,116 @@ function SingleNote({ note ,openModal, close,update,setUpdate}) {
       setText(value);
     }
   }
-  function handlePin(note) {
-    if(note.isPinned===true){
-      axios
-      .patch(`http://localhost:5000/api/editnote/${note._id}`, {
-        isPinned: false,
-      }).then(()=>{
-        note.isPinned=false;
-      })
-    }else{
-      axios
-      .patch(`http://localhost:5000/api/editnote/${note._id}`, {
-        isPinned: true,
-      }).then(()=>{
-        note.isPinned=true;
-      })
-    }
-    
-  }
 
-  function handleAdd(note) {
-    axios
-      .patch(`http://localhost:5000/api/editnote/${note._id}`, {
-        Title: title,
-        Tagline: tagline,
-        Text: text,
-        isPinned: ispinned,
-      })
-      .then((res) => {
-        setTagline("");
-        setText("");
-        setTitle("");
-        handleModalClose();
-      });
+  function handleEdit(note) {
+    if(title==="" && tagline==="" && text===""){
+      notify1();
+    }
+else{
+  axios
+  .patch(`http://localhost:5000/api/editnote/${note._id}`, {
+    Title: title,
+    Tagline: tagline,
+    Text: text,
+    isPinned: note.ispinned,
+  })
+  .then((res) => {
+    let b = notes.findIndex((a) => a._id === note._id);
+    var dummynotes = [...notes];
+    dummynotes[b].Title = title;
+    dummynotes[b].Tagline= tagline;
+    dummynotes[b].Text = text;
+
+    setNotes(dummynotes);
+    setTagline("");
+    setText("");
+    setTitle("");
+    handleModalClose();
+  });
+}
+    
   }
 
   return (
     <div>
-    <Modal
-      show={modalShow}
-      onHide={() => {
-        setModalShow(false);
-      }}
-    >
-      
-      <Modal.Body>
-        <div style={{ width: "480px" }}>
-        <div className="edit_note">
-        <BsPin onClick={handlePin} className="pin_icon" />
-        <input
-          className={theme?"add_input":'add_input_dark'}
-          type="text"
-          name="title"
-          onChange={(e) => {
-            handleChange(e);
-          }}
-          placeholder="Take a note..."
-          value={title}
+      <Modal
+        show={modalShow}
+        onHide={() => {
+          setModalShow(false);
+        }}
+      >
+        <Modal.Body>
+          <div style={{ width: "480px" }}>
+            <div className="edit_note">
+              <BsPin onClick={() => pinNote(note)} className="pin_icon" />
+              <input
+                className={theme ? "edit_input" : "edit_input_dark"}
+                type="text"
+                name="title"
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+                placeholder="Take a note..."
+                value={title}
+              />
+              <div className="edit_inputs">
+                <input
+                  className="edit_input1"
+                  type="text"
+                  name="tagline"
+                  onChange={(e) => {
+                    handleChange(e);
+                  }}
+                  placeholder="Enter a Tagline"
+                  value={tagline}
+                />
+                <input
+                  className="edit_input2"
+                  type="textarea"
+                  name="text"
+                  onChange={(e) => {
+                    handleChange(e);
+                  }}
+                  placeholder="Enter Note"
+                  value={text}
+                />
+                <div className="note_buttons">
+                  <button onClick={() => handleEdit(note)} className="add_btn">
+                    Edit
+                  </button>
+                  <button onClick={handleModalClose} className="close_btn">
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+      <div
+       
+        className={
+          view
+            ? `singlenote ${theme ? "" : `dark`}`
+            : `singlenote full ${theme ? `` : `dark`}`
+        }
+      >
+      <div onClick={()=>handleOpen(note)} className="open_div"></div>
+        <BsPin
+          onClick={() =>{ pinNote(note)}}
+          className={note.isPinned ? `pinned_icon ${theme? " ":" dark"}` : "pin_iconn"}
         />
-        <div className="edit_inputs">
-          <input
-            className="add_input1"
-            type="text"
-            name="tagline"
-            onChange={(e) => {
-              handleChange(e);
-            }}
-            placeholder="Enter a Tagline"
-            value={tagline}
-          />
-          <input
-            className="add_input2"
-            type="textarea"
-            name="text"
-            onChange={(e) => {
-              handleChange(e);
-            }}
-            placeholder="Enter Note"
-            value={text}
-          />
-          <div className="note_buttons">
-            <button onClick={()=>handleAdd(note)} className="add_btn">
-              Edit
-            </button>
-            <button onClick={handleModalClose} className="close_btn">
-              Close
-            </button>
+        <div className="dots">
+          <div onClick={() => deletee(note._id)}>
+            <IoTrashOutline />
           </div>
         </div>
-      </div>
-      </div>
-      </Modal.Body>
-    </Modal>
-    <div
-     
-      className={
-        view
-          ? `singlenote ${theme ? "" : `dark`}`
-          : `singlenote full ${theme ? `` : `dark`}`
-      }
-    >
-
-      <BsPin onClick={()=>handlePin(note)} className={note.isPinned?"pinned_icon":"pin_iconn"} />
-      <div className="dots">
-        <div onClick={handleDelete}>
-          <IoTrashOutline />
+        <h1>{note.Title}</h1>
+        <h3>{note.Tagline}</h3>
+        <div>
+          <p>{note.Text}</p>
         </div>
       </div>
-      <h1>{note.Title}</h1>
-      <h3>{note.Tagline}</h3>
-      <div>
-        <p>{note.Text}</p>
-      </div>
-    </div>
     </div>
   );
 }
